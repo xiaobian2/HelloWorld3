e85742de8116071746fe60b4f6c3bea03ea9d9fc
xiaobian2 <2931725715@qq.com>
2017-08-04 09:21:55 +0800

    这是第三个上传gitHub

---
+++ src/test/java/HWorld3.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+
+/**
+ * Created by Administrator on 2017/8/4.
+ */
+public class HWorld3 {
+    @Test
+    public void hWorldTest3() {
+        System.out.println("Hello World");
+    }
+}
