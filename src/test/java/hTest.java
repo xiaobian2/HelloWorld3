0cababdbd35e451c48749710335fbf434525ed95
xiaobian2 <2931725715@qq.com>
2017-08-04 09:28:01 +0800

    这是第一附加文件

---
+++ src/test/java/hTest.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+
+/**
+ * Created by Administrator on 2017/8/4.
+ */
+public class hTest {
+    @Test
+    public void hTest() {
+        System.out.println("Hello World");
+    }
+}
